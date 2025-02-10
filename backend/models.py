60d471079bd7ac9da32e372f39820941e805c4fd
waszkowitobi <tobiasz.waszkowiak@wat.edu.pl>
2025-02-10 15:12:29 +0100

    Update model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, mapped_column
 
@@ -7,9 +7,9 @@ Base = declarative_base()
 class Driver(Base):
     __tablename__ = 'driver'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    surname = Column(String, unique=True, index=True)
-    account_balance = Column(Numeric, unique=True, index=True)
+    name = Column(String, index=True)
+    surname = Column(String, index=True)
+    account_balance = Column(Numeric, index=True)
 
 class Car(Base):
     __tablename__ = 'car'
@@ -20,3 +20,14 @@ class Car(Base):
     registration_number = Column(String, unique=True, index=True)
 
     fk_driver = relationship("Driver", back_populates="cars")
+
+class Car(Base):
+    __tablename__ = 'ticket'
+    id = Column(Integer, primary_key=True, index=True)
+    car_id = mapped_column(ForeignKey("car.id"))
+    entrance_date = Column(String, index=True)
+    exit_date = Column(String, index=True)
+    amount = Column(Numeric, index=True)
+    payed = Column(Boolean, index=True)
+
+    fk_car = relationship("Car", back_populates="tickets")
